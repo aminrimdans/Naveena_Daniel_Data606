49f66b657546ce4d3577e4c8ba29fe5f73777dcd
NaveenaChodayy <106713975+NaveenaChodayy@users.noreply.github.com>
2022-08-10 22:36:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ dataset = st.container()
 features = st.container()
 model_training = st.container()
 
-
-
 with header:
 	st.title('FDA Recalls Project')
 
@@ -36,9 +34,6 @@ with dataset:
 if display_data:
     st.write(recalls_data.head())
 
-
-
-
 with features:
 	st.header('Parameters from Reason for Recall Text Column')
 
@@ -47,8 +42,6 @@ with features:
 	lol = recalls_data['Event Classification'].astype('category')
 	recalls_data['Event_indexed']=lol.cat.codes
 	st.write(recalls_data['Event_indexed'].head())
-
-
 	recalls_data['Reason for Recall'] = recalls_data['Reason for Recall'].apply(str.lower)
 	recalls_data['alpha check'] = recalls_data['Reason for Recall'].str.isalpha()
 	stopwords = stopwords.words('english')
@@ -65,62 +58,12 @@ with features:
 
 	original = vec_input(recalls_data['Reason for Recall'])
 
-
 with model_training:
-	st.header('ML model training using recalls dataset')
-
-	st.subheader('* **Parameter 1:** N_estimators')
-	n_estimators = st.slider('Please choose the number of trees in the random forest classification model',min_value=10, max_value=120, value=20, step=10)
-
-	st.subheader('* **Parameter 3:** Leaf Split')
-	min_samples_leaf = st.selectbox('Please choose the minimum number of samples that can be stored in leaf node', options=[1,3,4,5],index=0)
-
-	st.subheader('* **Parameter 4:** Sample Split')
-	min_samples_split = st.selectbox('Please choose the minimum number of samples required to split the internal node', options=[2,6,10],index=0)
-
-	
-
 	y = recalls_data['Event_indexed']
 	x = original
 	x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=.2, random_state=45)
-    
 
-	rf = RandomForestClassifier(n_estimators=n_estimators,min_samples_leaf=min_samples_leaf,min_samples_split=min_samples_split)
-	rf.fit(x_train,y_train)
-
-	prediction = rf.predict(x_test)
-
-
-
-	st.subheader('Mean Absolute Error of the model is:')
-	st.write(mae(y_test, prediction))
-
-	st.subheader('Mean Squared Error of the model is:')
-	st.write(mse(y_test, prediction))
-
-	st.subheader('R Mean Squared Error of the model is:')
-	st.write(r2_score(y_test, prediction))
-
-	st.subheader('Accuracy score is:')
-	st.write(accuracy_score(y_test, prediction))
-
-	st.header('Recall classification based on Random Forest Classifier model')
-	st.subheader('get inputs from the user') 
-
-
-	# Build the plot
-
-#sns.set(font_scale=1.4)
-#sns.heatmap(cm, annot=True, annot_kws={'size':10}, cmap=plt.cm.Greens, linewidths=0.2)
-
-metrics = st.sidebar.multiselect("Choose evaluation metrics :", ('Confusion Matrix', 'ROC Curve'))
-
-#st.subheader('Confusion Matrix')
-#plot_confusion_matrix(rf, X_test, y_test)
-#st.pyplot()
-
-def plot_metrics(metrics_list):
-	if 'Confusion Matrix' in metrics_list:
+	def plot_cfmatrix(prediction,y_test):
 		st.subheader("Confusion Matrix")
 		fig5 = plt.figure()
 		conf_matrix = confusion_matrix(prediction , y_test)
@@ -130,15 +73,70 @@ def plot_metrics(metrics_list):
 		plt.xlabel("Predicted")
 		st.pyplot(fig5)
 
+	#add code to display x_test wrt to pred randomly.
+	def display_pred(prediction,y_test):
+		#dataframe for ytest
+		original_df = recalls_data['Reason for Recall']
+
+		#dataframe for pred 
+		Predicted_df = pd.DataFrame(prediction)
+		Predicted_df.index = pd.RangeIndex(start=47071, stop=47071+len(Predicted_df), step=1)
+		#merge df based on index 
+		new_df = pd.merge(Predicted_df, original_df, left_index=True, right_index=True)
+		new_df.columns = ['Predicted Class', 'Reason for Recall']
+		pd.set_option('display.max_colwidth', 2)
+
+		output_df = new_df.sample()
+		st.write("Reason for Recall:",output_df['Reason for Recall'],"\n Predicted Class:",output_df['Predicted Class'])
+
+	mlmodel = st.sidebar.multiselect("Choose the model :", ('Logistic Regression', 'K-Nearest Neighbor', 'Random Forest Classifier'))
+
+	def select_model(model_list):
+		if 'Random Forest Classifier' in model_list:
+
+			st.header('ML model training using recalls dataset')
+			st.subheader('Random Forest Classifier')
+			st.subheader('* **Parameter 1:** N_estimators')
+			n_estimators = st.slider('Please choose the number of trees in the random forest classification model',min_value=10, max_value=120, value=20, step=10)
+			st.subheader('* **Parameter 3:** Leaf Split')
+			min_samples_leaf = st.selectbox('Please choose the minimum number of samples that can be stored in leaf node', options=[1,3,4,5],index=0)
+			st.subheader('* **Parameter 4:** Sample Split')
+			min_samples_split = st.selectbox('Please choose the minimum number of samples required to split the internal node', options=[2,6,10],index=0)
+			rf = RandomForestClassifier(n_estimators=n_estimators,min_samples_leaf=min_samples_leaf,min_samples_split=min_samples_split)
+			rf.fit(x_train,y_train)
+			prediction = rf.predict(x_test)
+			st.subheader('Accuracy score is:')
+			st.write(accuracy_score(y_test, prediction))
+			plot_cfmatrix(prediction,y_test)
+			st.header('Random sample for Recall classification based on Random Forest Classifier model')
+			display_pred(prediction,y_test)	
+
+		if 'Logistic Regression' in model_list:
+			st.subheader('Logistic Regression')
+			lr = LogisticRegression(max_iter=30000)
+			lr.fit(x_train,y_train)
+			prediction = lr.predict(x_test)
+			st.subheader('Accuracy score is:')
+			st.write(accuracy_score(y_test, prediction))
+			plot_cfmatrix(prediction,y_test)
+			st.header('Random sample for Recall classification based on logistic regression model')
+			display_pred(prediction,y_test)
+
+		if 'K-Nearest Neighbor' in model_list:
+			st.subheader('K-Nearest Neighbor')
+			knn = KNeighborsClassifier()
+			knn.fit(x_train,y_train)
+			prediction = knn.predict(x_test)
+			st.subheader('Accuracy score is:')
+			st.write(accuracy_score(y_test, prediction))
+			plot_cfmatrix(prediction,y_test)
+			st.header('Random sample for Recall classification based on K-Nearest Neighbor model')
+			display_pred(prediction,y_test)
 
 
-	if 'ROC Curve' in metrics_list:
-		st.subheader("ROC Curve")
-		plot_roc_curve(rf, prediction, y_test)
-		st.pyplot()
 
+select_model(mlmodel)
 
-plot_metrics(metrics)
 
 
 
