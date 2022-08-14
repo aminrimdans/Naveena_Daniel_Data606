d28984142728b145284e3e97e4eeb0a3cb1cd9f4
NaveenaChodayy <106713975+NaveenaChodayy@users.noreply.github.com>
2022-08-14 21:57:02 +0530

    Update project.py

--- Streamlit/project.py
+++ Streamlit/project.py
@@ -2,6 +2,8 @@ import openpyxl
 import streamlit as st 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import sklearn
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
